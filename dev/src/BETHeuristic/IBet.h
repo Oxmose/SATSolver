990f5d30b3eaa9f8c9261dff140d610bca7ec23e
Oxmose <alexy.torresa@gmail.com>
2016-03-09 11:14:00 +0100

    Interace

---
+++ dev/src/BETHeuristic/IBet.h
@@ -0,0 +1,19 @@
+/*
+ *
+ *  INTERFACE BET
+ *
+*/
+
+#ifndef DEF_IBET_H
+#define DEF_IBET_H
+
+class IBet
+{
+    public:
+        virtual ~Bet() = 0;
+        virtual void toOverride() = 0;
+
+    private:
+}; // IBet
+
+#endif // DEF_IBET_H
