e0f7d5ec7fb8640bcad746c0bf644e4bf4660ef4
Oxmose <alexy.torresa@gmail.com>
2016-03-18 23:23:51 +0100

    Test time

---
+++ dev/bin/time_set_test_unsat.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#Set for unsat
+echo "{"
+echo "    \"UNSAT\": ["
+
+for f in test_base/cnf/serial_test/unsat/*/*.cnf
+do
+    echo "        {"
+    echo "            \"file\": \"$f\"," | sed s/test_base\\/cnf\\/serial_test\\/unsat\\///
+
+    #STD
+    (/usr/bin/time -f "%e r" ./resol $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"std\":\ \"/ | sed s/$/\",/
+    #WL
+    (/usr/bin/time -f "%e r" ./resol -wl $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"wl\":\ \"/ | sed s/$/\",/
+
+    #RAND
+    #(/usr/bin/time -f "%e r" ./resol -rand $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"rand\":\ \"/ | sed s/$/\",/
+    #RAND0
+    #(/usr/bin/time -f "%e r" ./resol -rand0 $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"rand0\":\ \"/ | sed s/$/\",/
+    #MOMS
+    (/usr/bin/time -f "%e r" ./resol -moms $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"moms\":\ \"/ | sed s/$/\",/
+    #DLIS
+    #(/usr/bin/time -f "%e r" ./resol -dlis $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"dlis\":\ \"/ | sed s/$/\",/
+    #DLIS0
+    #(/usr/bin/time -f "%e r" ./resol -dlis0 $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"dlis0\":\ \"/ | sed s/$/\",/
+
+    #HBET + WL
+    #RAND
+    #(/usr/bin/time -f "%e r" ./resol -rand -wl $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"randwl\":\ \"/ | sed s/$/\",/
+    #RAND0
+    #(/usr/bin/time -f "%e r" ./resol -rand0 -wl $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"rand0wl\":\ \"/ | sed s/$/\",/
+    #MOMS
+    (/usr/bin/time -f "%e r" ./resol -moms -wl $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"momswl\":\ \"/ | sed s/$/\",/
+    #DLIS
+    #(/usr/bin/time -f "%e r" ./resol -dlis -wl $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"dliswl\":\ \"/ | sed s/$/\",/
+    #DLIS0
+    #(/usr/bin/time -f "%e r" ./resol -dlis0 -wl $f) |& grep r | sed s/\ r// | sed s/^/\ \ \ \ \ \ \ \ \ \ \ \ \"dlis0wl\":\ \"/ | sed s/$/\"/
+    echo "        },"
+done
+echo "        { \"file\": \"end\" }"
+echo "    ]"
+
+echo "}"
+
+
+
