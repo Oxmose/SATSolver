4a3f0043586cb1a2ffb1fce0c1215a7533e0197b
Alexy Torres <alexy@alexytens>
2016-03-22 23:15:37 +0100

    Readme updated

--- dev/bin/test_base/cnf/serial_test/unsat/clean_base.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for f in *.cnf
-do
-echo $f
-sed -i -e s/^0$\// -e s/%// $f
-done
--- dev/bin/test_total_time.sh
+++ dev/bin/test_total_time.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Launching time tests for SAT without whatched literals"
-test_time/time_set_test_sat_nowl.sh > test_time/result_sat_nowl &
+test_time/time_set_test_sat_nowl.sh > results/test_time/result_sat_nowl
 
 echo "Launching time tests for SAT with whatched literals"
-test_time/time_set_test_sat_wl.sh > test_time/result_sat_wl &
+test_time/time_set_test_sat_wl.sh > results/test_time/result_sat_wl
 
 echo "Launching time tests for UNSAT without whatched literals"
-test_time/time_set_test_unsat_nowl.sh > test_time/result_unsat_nowl &
+test_time/time_set_test_unsat_nowl.sh > results/test_time/result_unsat_nowl
 
 echo "Launching time tests for UNSAT with whatched literals"
-test_time/time_set_test_unsat_wl.sh > test_time/result_unsat_wl &
+test_time/time_set_test_unsat_wl.sh > results/test_time/result_unsat_wl
