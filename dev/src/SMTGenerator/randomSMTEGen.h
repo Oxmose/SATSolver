571bd29b26d1f7fe7f18161d32cb7f564ac631a7
Oxmose <alexy.torresa@gmail.com>
2016-05-10 14:48:32 +0200

    SMTE generator V1

--- dev/src/SMTGenerator/randomSMTEGen.h
+++ dev/src/SMTGenerator/randomSMTEGen.h
@@ -15,6 +15,44 @@
 #include <ctime>       	// std::time
 #include <fstream>     	// std::ofstream
 
+enum OPERATOR
+{
+    IMPL = 0,
+    EQUI = 1,
+    OR   = 2,
+    AND  = 3,
+    XOR  = 4,
+    EQUA = 5,
+    DIFF = 6,
+    NONE = 7
+};
+
+enum TYPE
+{
+    OP = 10,
+    LIT = 11
+};
+
+typedef struct node node;
+struct node
+{    
+    TYPE type;
+    node *left;
+    node *right;
+    int lit;
+    OPERATOR op;
+    node(TYPE t, node* l, node* r, OPERATOR o = NONE, int lite = 0)
+    {
+        type = t;
+        left = l;
+        right = r;
+        op = o;
+        lit = lite;
+    }
+};
+
 void randomGenerator(const std::string &p_fileName);
+void generateTree(unsigned int maxIndex, unsigned int clausesCount, unsigned int clausesSize, std::ofstream &out);
+void dfs(node *root, std::ofstream &out);
 
 #endif // DEF_RANDOMSMTEGENERATOR_H
